aa9d1c1bded431c8d13706867483749acfaaa000
曾缘 <zengyuan@tuhu.cn>
2019-04-17 20:20:49 +0800

    feat: 发帖页面2.0

--- src/pages/article/post.js
+++ src/pages/article/post.js
@@ -1,7 +1,38 @@
+const util = require('../../lib/util');
 Page({
   data: {
     navTitle: '发布',
     detail: null,
+    tabName: '全部', // 分类名称
   },
+  onLoad(){
 
+  },
+  changeTab() {
+    console.log('点击')
+    wx.showActionSheet({
+      itemList: Object.keys(util.listTabs).map(item => {
+        return util.listTabs[item];
+      }),
+      success: (res) => {
+        // wx.pageScrollTo({
+        //   scrollTop: 0,
+        //   duration: 400
+        // });
+        const tab = Object.keys(util.listTabs)[res.tapIndex];
+        if (tab !== this.tab) {
+          this.tab = tab;
+          this.setData({
+            tabName: util.tabToWord(tab) || ''
+          });
+        }
+      },
+      fail(res) {
+        console.log(res.errMsg)
+      }
+    })
+  },
+  dianji(){
+    console.log('点击输入框')
+  }
 })
